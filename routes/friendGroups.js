fcf75392644dbb0aa2a41125db8fa0216e2dcb18
John Florkey <john.florkey@gmail.com>
2021-11-01 22:55:50 -0400

    moved friend group movie recommendations to its own route

--- routes/friendGroups.js
+++ routes/friendGroups.js
@@ -7,32 +7,23 @@ const MediaItem = require("../models/MediaItem");
 router.get("/", async (req, res, next) => {
   try {
     const response = await axios.get(`http://localhost:3001/friendgroups/`);
-    const responseData = {};
-    response.data.map((g) => {
-      responseData[g.id] = { ...g };
-      delete responseData[g.id].movierecommendations;
-      delete responseData[g.id].tvrecommendations;
-      responseData[g.id].movieRecommendations = {};
-      responseData[g.id].tvRecommendations = {};
-      g.movierecommendations.map(
-        (r) =>
-          (responseData[g.id].movieRecommendations[r.id] = MediaItem.factory(
-            r,
-            "movies"
-          ))
-      );
-      g.tvrecommendations.map(
-        (r) =>
-          (responseData[g.id].tvRecommendations[r.id] = MediaItem.factory(
-            r,
-            "tv"
-          ))
-      );
-    });
 
-    return res.json(responseData);
+    return res.json(response.data);
   } catch (e) {
     throw new ExpressError("Bad request", 400);
+  }
+});
+
+router.get("/:id/movies", async (req, res, next) => {
+  try {
+    const friendGroupID = req.params.id;
+    const response = await axios.get(
+      `http://localhost:3001/movierecommendations/${friendGroupID}`
+    );
+
+    return res.json(response.data);
+  } catch (err) {
+    throw new ExpressError("Bad Request", 404);
   }
 });
 
