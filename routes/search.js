bbee1ce2ceb7eab89cfd770d3681a2a3a03ecd8f
John Florkey <john.florkey@gmail.com>
2021-11-08 16:16:24 -0500

    refactored search route to use TMDB API

--- routes/search.js
+++ routes/search.js
@@ -3,21 +3,38 @@ const router = new express.Router();
 const axios = require("axios");
 const ExpressError = require("../expressError");
 const MediaItem = require("../models/MediaItem");
+const Movie = require("../models/Movie");
+const TV = require("../models/TV");
+const { TMDB_API_BASE_URL } = require("../config");
+const token = process.env.TMDB_BEARER_TOKEN;
 
 router.get("/:type", async (req, res, next) => {
   try {
     const type = req.params.type;
     const searchTerm = req.query.title;
     const requestedPage = req.query.page;
-    const resource = `search${type}`;
-    const response = await axios.get(`http://localhost:3001/${resource}`, {
-      params: { page: requestedPage },
-    });
-    const responseData = response.data[0];
-    responseData.searchTerm = searchTerm;
-    responseData.results = responseData.results.map((r) =>
-      MediaItem.factory(r, type)
+    const resource = type === "movies" ? "movie" : type;
+    const response = await axios.get(
+      `${TMDB_API_BASE_URL}/3/search/${resource}`,
+      {
+        params: { page: requestedPage, query: searchTerm },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
+    const responseData = response.data;
+    responseData.searchTerm = searchTerm;
+    if (type === "movies") {
+      responseData.results = responseData.results.map((r) =>
+        MediaItem.factory(new Movie(r))
+      );
+    } else if (type === "tv") {
+      responseData.results = responseData.results.map((r) =>
+        MediaItem.factory(new TV(r))
+      );
+    }
+
     return res.json(responseData);
   } catch (e) {
     throw new ExpressError("Bad request", 400);
