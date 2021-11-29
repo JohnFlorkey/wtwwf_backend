344b0d28681ac6da8eee85c826dade6e81829f3e
John Florkey <john.florkey@gmail.com>
2021-11-28 22:06:58 -0500

    changes for prod

--- db.js
+++ db.js
@@ -1,7 +1,18 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
-const client = new Client(DB_URI);
+let client;
+
+if (process.env.NODE_ENV === "production") {
+  client = new Client({
+    connectionString: DB_URI,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  client = new Client(DB_URI);
+}
 
 client.connect();
 
